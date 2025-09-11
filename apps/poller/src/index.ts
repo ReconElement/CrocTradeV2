0f541f31052f072ddcfffc488327197bc93ede64
ReconElement <132275628+ReconElement@users.noreply.github.com>
2025-09-12 01:22:04 +0530

    Added some code
    Added some codee

--- apps/poller/src/index.ts
+++ apps/poller/src/index.ts
@@ -1,17 +1,105 @@
-import WebSocket from "ws";
-const webSocketURL = new WebSocket("wss://ws.backpack.exchange");
-async function getWSData(){
+import {createClient} from 'redis';
+import WebSocket from 'ws';
+const webSocketUri = new WebSocket("wss://ws.backpack.exchange");
+const redisClient = createClient({
+    url: "redis://localhost:6379"
+});
+
+try{
+    await redisClient.connect();
+}catch(e){
+    console.log(`Some error ocurred while connecting to the redis client: ${e}`);
+}
+
+async function getDataWS(){
+    let dataCollection: WebSocket.RawData[] = [];
     const subscribeMessage = {
         method: "SUBSCRIBE",
-        params: ["bookTicker.ETH_USDC","bookTicker.SOL_USDC","bookTicker.BTC_USDC"]
+        params: ["bookTicker.ETH_USDC", "bookTicker.SOL_USDC","bookTicker.BTC_USDC"]
     };
-    webSocketURL.on('open',()=>{
-        webSocketURL.send(JSON.stringify(subscribeMessage));
-        webSocketURL.on("message",(data)=>{
-            console.log(JSON.parse(data.toString()));
-        })
+    webSocketUri.on('open',()=>{
+        webSocketUri.send(JSON.stringify(subscribeMessage));
+    });
+    webSocketUri.on('message',async (data)=>{
+        dataCollection.push(data);
+        if(dataCollection.length>=100){
+            await collectDataAndSend(dataCollection);
+            dataCollection = [];
+        }
     })
 };
 
-await getWSData();
-//TODO: Have to send the latest amount of all the three coins between a duration of 100ms over Redis stream to engine
+await getDataWS();
+
+type recievedValue = {
+    SOL_USDC: {
+        asset: "SOL_USDC",
+        price: number,
+        decimal: number,
+    },
+    BTC_USDC: {
+        asset: "BTC_USDC",
+        price: number,
+        decimal: number
+    },
+    ETH_USDC: {
+        asset: "ETH_USDC",
+        price: number,
+        decimal: number
+    }
+}
+async function collectDataAndSend(data: WebSocket.RawData[]){
+    let obj: recievedValue = {
+        SOL_USDC: {
+            asset: "SOL_USDC",
+            price: 0,
+            decimal: 0
+        },
+        BTC_USDC: {
+            asset: "BTC_USDC",
+            price: 0,
+            decimal: 0
+        },
+        ETH_USDC: {
+            asset: "ETH_USDC",
+            price: 0,
+            decimal: 0
+        }
+    };
+    for(let i=data.length-1;i>=60;i--){
+        // console.log(JSON.parse(data[i]?.toString()));
+        const value = JSON.parse(data[i]?.toString()??" ");
+        if(value?.data?.s==="SOL_USDC"){
+            obj.SOL_USDC = {
+                asset: "SOL_USDC",
+                price: Number(Math.trunc(value?.data?.b*10000)),
+                decimal: 4
+            }
+        }
+        if(value?.data?.s==="BTC_USDC"){
+            obj.BTC_USDC = {
+                asset: "BTC_USDC",
+                price: Number(Math.trunc(value?.data?.b*10000)),
+                decimal: 4
+            }
+        }
+        if(value?.data?.s==="ETH_USDC"){
+            obj.ETH_USDC = {
+                asset: "ETH_USDC",
+                price: Number(Math.trunc(value?.data?.b*10000)),
+                decimal: 4
+            }
+        }
+    };
+    try{
+        setInterval(async ()=>{
+            const res = await redisClient.xAdd("stream","*",{
+                data: JSON.stringify(obj)
+            });
+            console.log(res);
+        },100)
+    }catch(e){
+        console.log(`Some error occurred while sending stream data on redis: ${e}`);
+    }
+};
+
